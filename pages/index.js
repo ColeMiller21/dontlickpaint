bdbde3521276bf023a63b010397301e14bc44935
Cole Miller <cole.t.miller@gmail.com>
2022-11-30 10:29:06 -0600

    fixed mobile text sizes

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ export default function Home() {
       <SEOMeta page="" description={SEODesc} />
       <div className="min-full-page bg-[#3a4247] flex flex-col items-center justify-center overflow-hidden">
         <div className="flex flex-col items-center relative">
-          <h1 className="text-[4rem] text-[#ff8e85] z-10 font-teko font-bold mb-[-2rem] md:text-[7rem] md:mb-[-5rem] lg:text-[9rem] lg:mb-[-6rem] ">
+          <h1 className="text-[3rem] text-[#ff8e85] z-10 font-teko font-bold mb-[-2rem] md:text-[7rem] md:mb-[-5rem] lg:text-[9rem] lg:mb-[-6rem] ">
             DO NOT BUY
           </h1>
-          <h1 className="text-[5rem] md:text-[9rem] lg:text-[12rem] text-white z-10 font-didot">
+          <h1 className="text-[4rem] md:text-[9rem] lg:text-[12rem] text-white z-10 font-didot">
             Lick Paint
           </h1>
           <img
